5108078423df979cd9ce155b9e528179865e5bef
Bertrand Sudre <sudrebertrand@gmail.com>
2022-12-23 23:47:01 +0100

    Ajouter confirmation quand envoi réussi Ajouter la fonctionnalité au bouton (x)

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,77 @@ function editNav() {
   }
 }
 
-// DOM Elements
+
+
+
+/* DOM ELEMENTS */
 const modalbg = document.querySelector(".bground");
+const modalBody = document.querySelector(".modal-body");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const submitBtn = document.querySelector(".btn-submit");
+const modalValidation = document.querySelector(".modal-validation");
+const modalCloseBtn = document.querySelector(".btn-close");
+// Form
+const form = document.getElementById('reserve');
+const firstname = document.getElementById('first');
+const lastname = document.getElementById('last');
+const email = document.getElementById('email');
+const birthday = document.getElementById('birthday');
+const quantity = document.getElementById('quantity');
+const tournament = document.getElementById('location1');
+const conditions = document.getElementById('checkbox1');
 
-// launch modal event
+/* EVENT */
+
+// Launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+
+
+// Submit modal
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); // ne pas exécuter par défaut
+  
+  hide(modalBody);
+  show(modalValidation);
+})
+
+// Close modal events
+closeBtn.addEventListener("click", closeModal);
+modalCloseBtn.addEventListener("click", closeModal);
+
+/* FUNCTION */
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// launch modal form
+function hide(division) {
+  division.style.display = "none";
+}
+
+// launch modal form
+function show(division) {
+  division.style.display = "block";
+}
+
+// launch modal form
+function closeModal() {
+  show(modalBody);
+  hide(modalValidation);
+  hide(modalbg);
+  form.reset();
+}
+
+
+
+
+
+
+
 
 
